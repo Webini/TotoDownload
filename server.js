ceb54b236e9ef7691f13d9eb7041930f8afd113b
Nicolas GASPARINI <nicocanicolas@gmail.com>
2015-04-09 19:02:02 +0200

    reactivation des workers

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var app = require('./app/app.js');
 app.server.listen(app.config.http.port, app.config.http.host, function(){
     console.log("Listening on " + app.config.http.host + ":" + app.config.http.port);
     
-    //app.workers.torrents.start();
-    //app.workers.sync.start();
+    app.workers.torrents.start();
+    app.workers.sync.start();
 });
