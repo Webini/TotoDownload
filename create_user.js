23751a779dc27c5d05dba973d0744d40e01e1002
root <root@ns362714.ip-91-121-176.eu>
2016-04-04 02:05:48 +0200

    fix

--- create_user.js
+++ create_user.js
@@ -2,14 +2,22 @@
 var app = require('./app/app.js');
 var UserService = app.services.UserService;
 var args = process.argv.slice(2);
+var email = args[1] || 'admin@admin.org';
 
 UserService.create({
             nickname: args[0] || 'admin',
-            email: args[1] || 'admin@admin.org',
+            email: email,
             ip: '127.0.0.1',
-            roles: UserService.roles.UPLOADER | UserService.roles.ADMIN | UserService.roles.SUPER_ADMIN | UserService.roles.TAGGER,
             password: args[2] || 'admin',
-            diskSpace: 200000
+}).then(function(){
+    return app.orm.User.findOne({ where: { email: email } });
+}).then(function(user){
+    if (user) {
+        user.roles = UserService.roles.UPLOADER | UserService.roles.ADMIN | UserService.roles.SUPER_ADMIN | UserService.roles.TAGGER;
+        user.diskSpace = 200000;
+        return user.save();
+    }
+    return 1;
 }).then(
     function(ok){
         console.log('User created');
