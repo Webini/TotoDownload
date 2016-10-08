1f20533a47e93b9c0e8be5bd38a0c1e33418c138
Nicolas <nicolas.gasp@free.fr>
2016-10-08 22:42:58 +0200

    bugfix

--- app/services/transcodingService.js
+++ app/services/transcodingService.js
@@ -3,4 +3,4 @@ const TotoTranscoder = require('toto-transcoder');
 module.exports = ['TranscodingService', (function(){
     var app = require(__dirname + '/../app.js');
     return new TotoTranscoder(app.config.ffmpeg);
-})];
+})()];
