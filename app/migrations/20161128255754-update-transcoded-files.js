f3ab4a7a5830e98fe519baeae50ad034a996386e
Nicolas <nicolas.gasp@free.fr>
2017-03-18 12:44:43 +0100

    fix migrations

--- app/migrations/20161128255754-update-transcoded-files.js
+++ app/migrations/20161128255754-update-transcoded-files.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-     return queryInterface.addColumn(
+     return queryInterface.changeColumn(
         'TranscodedFiles',
         'thumbsMeta',
         {
